13e2fc9c7153de29274169501bb32f0df3ee5a44
Hans Combee <39181810+hcombee@users.noreply.github.com>
2019-12-26 06:55:30 +0100

    Update ConkyMusic.py

--- ConkyMusic.py
+++ ConkyMusic.py
@@ -6,6 +6,25 @@
 # Tested on Ubuntu with Clemetine and Spotify
 # Depends on python3 dbus and mpris2 for Python3
 #  
+#  Copyright 2019 Hans Combee (hanscombee@gmail.com)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# Version history
+# 0.5 Initial upload
 
 import dbus
 import os
